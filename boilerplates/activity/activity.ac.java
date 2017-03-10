6d081b918458beca6d8350177309dac7dd5ed824
endlessdev <ysw0094@gmail.com>
2017-03-11 00:58:30 +0900

    Add Boilerplate files needed for Fragment

--- boilerplates/activity/activity.ac.java
+++ boilerplates/activity/activity.ac.java
@@ -8,6 +8,6 @@ public class {%activityName%}Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_{%activityNameLowerCase%}});
+        setContentView(R.layout.activity_{%activityNameLowerCase%});
     }
 }
